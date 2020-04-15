3775325ca60a6c2c703251534f699301a6b3b11c
Shelmith-Kariuki <kariukishelmith@gmail.com>
2020-04-15 19:51:56 +0300

    Initial package files

---
+++ R/V1_T2_2.R
@@ -0,0 +1,12 @@
+#' Table 2. 2: Distribution of Population by Sex and County
+#'
+#'
+#' @docType data
+#'
+#' @usage data(V1_T2.2)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @examples Coming soon
