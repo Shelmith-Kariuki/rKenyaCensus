1ed2eaaf6b0eb3e8b33976307242c6a872e42660
Shelmith-Kariuki <kariukishelmith@gmail.com>
2020-04-16 02:59:01 +0300

    Added Volume 1, Table 2.6

---
+++ R/V1_T2.6.R
@@ -0,0 +1,19 @@
+#' Volume I: Table 2.6
+#'
+#' Distribution of Population, Number of Households and Average Household Size by Sub-County
+#' @docType data
+#'
+#' @usage data(V1_T2.6)
+#'
+#' @format A data frame with 6 variables:
+#' \describe{
+#' \item{\code{County}}{County}
+#' \item{\code{SubCounty}}{Sub County}
+#' \item{\code{AdminArea}}{Shows whether the area is a County or SubCounty}
+#' \item{\code{Population}}{Population Size}
+#' \item{\code{NumberOfHouseholds}}{Number of Households}
+#' \item{\code{AverageHouseholdSize}}{Average Household Size}
+#'}
+#' @keywords datasets
+#'
+"V1_T2.6"
